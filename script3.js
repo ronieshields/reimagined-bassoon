f0ec309aa0b1c349637e7994c3db74a2d89442ea
josh madison <justjoshinyac@gmail.com>
2024-11-25 16:19:50 -0800

    initial commit

---
+++ script3.js
@@ -0,0 +1,20 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.buttons button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonText = button.textContent;
+
+        if (buttonText === '=') {
+            try {
+                display.value = eval(display.value);
+            } catch (error) {
+                display.value = 'Error';
+            }
+        } else if (buttonText === 'C') {
+            display.value = '';
+        } else {
+            display.value += buttonText;
+        }
+    });
+});
